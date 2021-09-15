c01343b7405b3d84ee572bd18ff33080da40adcb
Joseph <joe@uhhuhohyeah.com>
2021-09-15 14:10:53 -0700

    added basic render framework for page -- need to finish card framework and test

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
+const path = require('path')
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 
 // Prompt user from list: Engineer, Intern, Manager
